eed3a3ce7a310aeb6bc23dc7211d5b4105ded1e7
Exiquio Cooper-Anderson <exiquio@gmail.com>
2024-08-26 21:55:18 -0600

    Configured for Ruby-ing

--- init.lua
+++ init.lua
@@ -569,17 +569,18 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        ruby_lsp = {},
+        --clangd = {},
         -- gopls = {},
-        pyright = {},
-        rust_analyzer = {},
+        --pyright = {},
+        --rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        -- tsserver = {},
         --
 
         lua_ls = {
@@ -860,6 +861,7 @@ require('lazy').setup({
         'markdown_inline',
         'python',
         'query',
+        'ruby',
         'rust',
         'vim',
         'vimdoc',
