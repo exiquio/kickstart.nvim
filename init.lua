b5a07a5496874ee0f864c78bd7fd897c941b8ad8
Exiquio Cooper-Anderson <exiquio@gmail.com>
2025-02-05 10:24:15 -0700

    Tweaking a little bit

--- init.lua
+++ init.lua
@@ -569,7 +569,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        ruby_lsp = {},
+        --ruby_lsp = {},
         --clangd = {},
         -- gopls = {},
         --pyright = {},
@@ -793,17 +793,23 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    'Mofiqul/dracula.nvim',
+    priority = 100,
     init = function()
+      vim.cmd.colorscheme 'dracula'
+    end,
+
+    --'folke/tokyonight.nvim',
+    --priority = 1000, -- Make sure to load this before all the other start plugins.
+    --init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+      --vim.cmd.hi 'Comment gui=none'
+    --end,
   },
 
   -- Highlight todo, notes, etc in comments
